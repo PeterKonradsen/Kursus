c4c89bf8ed2f3892540add4af94735b8c4fb0335
Peter Konradsen (PKO) <PKO@kmd.dk>
2020-04-16 14:22:30 +0200

    Add project files.

---
+++ ALegeplads/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ALegeplads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
